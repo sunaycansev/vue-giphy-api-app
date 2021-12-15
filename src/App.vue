50365dfff6ad50e12f41522429c6fe2dee4cfe40
sunaycansev <fsunaycansev@gmail.com>
2021-12-15 15:29:01 +0300

    add search component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <search></search>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Search from "@/components/Search";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Search },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search">
+    <input type="text" v-model="query" />
+    <button @click="handleSearch">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    handleSearch() {
+      console.log(this.query);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
